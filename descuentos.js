92cea7b0c017ddddfe37ac7add8a88f9155b4f8f
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-05-06 16:36:10 -0500

    Descuentos con cupón

--- descuentos.js
+++ descuentos.js
@@ -9,11 +9,29 @@ function calcularPrecioConDescuento(precio, descuento){
 function onClickButtonPriceDiscount(){
     const inputPrice=document.getElementById("InputPrice");
     const priceValue=inputPrice.value;  
-    const inputDiscount=document.getElementById("InputDiscount");
-    const discountValue=inputDiscount.value;
-    const precioConDescuento=calcularPrecioConDescuento(priceValue, discountValue);
+    //const inputDiscount=document.getElementById("InputDiscount");
+    //const discountValue=inputDiscount.value;
+    const inputCoupon=document.getElementById("InputCoupon");
+    const couponValue=inputCoupon.value;
+    
+    //const precioConDescuento=calcularPrecioConDescuento(priceValue, discountValue);
+    //const resultP=document.getElementById("ResultP");
+    //resultP.innerText="El precio con descuento son: $"+precioConDescuento;
+    let descuento;
+        switch(couponValue){
+            case coupons[0]:     //Platino
+            descuento=15;
+            break;
+            case coupons[1]:     //Diamante
+            descuento=30;
+            break;
+            case coupons[2]:     //Dorado
+            descuento=45;
+            break;
+                          }
+    const precioConDescuento=calcularPrecioConDescuento(priceValue, descuento);
     const resultP=document.getElementById("ResultP");
-    resultP.innerText="El precio con descuento son: $"+precioConDescuento;
+    resultP.innerText="El precion con descuento es: $"+ precioConDescuento;
 }
 
 
@@ -23,3 +41,9 @@ function onClickButtonPriceDiscount(){
 //    porcentajePrecioConDescuento,
 //    precioConDescuento,
 //});
+
+const coupons=[
+    "Platino",
+    "Diamante",
+    "Dorado",
+];
