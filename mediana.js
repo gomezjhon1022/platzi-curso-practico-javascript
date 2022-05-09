48d0f896b5cc3b3877697ccda87b46fde73998bb
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-05-09 13:48:06 -0500

    Moda

--- mediana.js
+++ mediana.js
@@ -1,35 +1,37 @@
-const lista1=[
-    100,
-    200,
-    500,
-    400000000,
-];
-function calcularMediaAritmetica(lista){
-     let sumaLista=0;
-     for (let i=0;i<lista.length;i++){
-     sumaLista=sumaLista+lista[i];
-     }
+function calcularMediana(lista1){
+    lista1.sort(function(a,b){return a - b});
+    console.log(lista1);
     
-    const promedioLista=sumaLista/lista.length;
-    return promedioLista;
+    function calcularMediaAritmetica(lista){
+         let sumaLista=0;
+         for (let i=0;i<lista.length;i++){
+         sumaLista=sumaLista+lista[i];
+         }
+        
+        const promedioLista=sumaLista/lista.length;
+        return promedioLista;
+        }
+    
+    const mitadLista1=parseInt(lista1.length/2);
+    function esPar(numerito){
+        if (numerito % 2===0){
+            return true;
+        } else {
+            return false;
+        }
     }
-
-const mitadLista1=parseInt(lista1.length/2);
-function esPar(numerito){
-    if (numerito % 2===0){
-        return true;
-    } else {
-        return false;
+    let mediana;
+    
+    if (esPar(lista1.length)){
+        const elemento1=lista1[mitadLista1-1];
+        const elemento2=lista1[(mitadLista1)];
+        const promedioElemento1y2=calcularMediaAritmetica([elemento1,elemento2]);
+    
+       mediana=promedioElemento1y2;
+    }  else{
+        mediana=lista1[mitadLista1];
     }
-}
-let mediana;
+    return mediana;
 
-if (esPar(lista1.length)){
-    const elemento1=lista1[mitadLista1-1];
-    const elemento2=lista1[(mitadLista1)];
-    const promedioElemento1y2=calcularMediaAritmetica([elemento1,elemento2]);
 
-   mediana=promedioElemento1y2;
-}  else{
-    mediana=lista1[mitadLista1];
 }
